aedcd2cba62d258ff5841880e25c12ed42090edf
= <=>
2021-10-24 09:24:09 +0500

    initial commit

---
+++ TransformerMaker.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Asim56\LaravelTransformerMaker;
+
+class TransformerMaker
+{
+    public function add($sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
